0c82b48b6024b96d14d91fec25d4ca3844837f6e
rzldin <rizaljihadudin@gmail.com>
2020-06-23 20:54:03 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sasambo-new';
+$config['base_url'] = 'http://localhost/websasambo';
 
 /*
 |--------------------------------------------------------------------------
